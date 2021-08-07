72e69eaff924f34f9636a67289819d212ee8a24b
Carlo <shiranoyukihime@gmail.com>
2021-08-07 17:17:33 +0800

    initial commit

---
+++ Assets/Libs/PhotonUnityNetworking/UtilityScripts/QuitOnEscapeOrBack.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitOnEscapeOrBack : MonoBehaviour
+{
+    private void Update()
+    {
+        // "backgroundFillImage" button of phone equals "Escape". quit app if that's pressed
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
